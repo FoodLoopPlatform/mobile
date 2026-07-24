5161f39de9405b42d47aea0b8f7bf5449d9c7ffb
salah3mer110 <salahamer3040@gmail.com>
2026-07-24 17:31:10 +0300

    Forget password view

---
+++ lib/features/auth/presentation/views/forgot_password_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodloop/core/utils/app_colors.dart';
+import 'package:foodloop/core/utils/app_strings.dart';
+import 'package:foodloop/features/auth/presentation/views/widgets/forgot_password_body.dart';
+
+class ForgotPasswordView extends StatelessWidget {
+  const ForgotPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(color: AppColors.primary),
+        title: Text(
+          AppStrings.appName,
+          style: TextStyle(
+            fontFamily: 'PlayfairDisplay',
+            fontSize: 24.sp,
+            fontWeight: FontWeight.w600,
+            color: AppColors.primary,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: const ForgotPasswordBody(),
+    );
+  }
+}
